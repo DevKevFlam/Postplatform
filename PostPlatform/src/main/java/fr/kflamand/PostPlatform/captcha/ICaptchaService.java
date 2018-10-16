91198fbfaa932712446bb3ad43fe52414ea4d999
GrAiDeR <Kev.flamand@gmail.com>
2018-10-16 15:10:55 +0200

    Ajout class pour captcha

---
+++ PostPlatform/src/main/java/fr/kflamand/PostPlatform/captcha/ICaptchaService.java
@@ -0,0 +1,11 @@
+package fr.kflamand.PostPlatform.captcha;
+
+import org.baeldung.web.error.ReCaptchaInvalidException;
+
+public interface ICaptchaService {
+    void processResponse(final String response) throws ReCaptchaInvalidException;
+
+    String getReCaptchaSite();
+
+    String getReCaptchaSecret();
+}
