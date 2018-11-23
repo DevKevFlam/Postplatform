4ac303264ff8ff20de793d1f40c6ea6401af8ca7
GrAiDeR <Kev.flamand@gmail.com>
2018-11-23 09:57:55 +0100

    Ajout dépendance AuthServer

--- Auth-server/src/main/java/fr/kflamand/Authserver/models/UserModel.java
+++ Auth-server/src/main/java/fr/kflamand/Authserver/models/UserModel.java
@@ -1,14 +1,27 @@
 package fr.kflamand.Authserver.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.security.core.GrantedAuthority;
 
+import javax.persistence.Column;
 import java.util.ArrayList;
 import java.util.Collection;
 
 public class UserModel {
 
+    @Column(unique = true)
     private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
+
+    private String fullName;
+
+    // Boolean de validation du compte sert a bloqué le login si false
+    private Boolean Enabled;
+
+
+
     private Collection<GrantedAuthority> grantedAuthoritiesList = new ArrayList<>();
 
     public String getUsername() {
